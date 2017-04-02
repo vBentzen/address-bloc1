efc8690669f8d090656bda69e4bedde3679ce759
Aske Bentzen <askebentzen@gmail.com>
2017-04-02 14:05:23 +0200

    me

--- models/address_book.rb
+++ models/address_book.rb
@@ -31,17 +31,12 @@ class AddressBook
 
 
   def binary_search(name)
-    # #1
     lower = 0
     upper = entries.length - 1
 
-    # #2
     while lower <= upper
-      # #3
       mid = (lower + upper) / 2
       mid_name = entries[mid].name
-
-      # #4
       if name == mid_name
         return entries[mid]
       elsif name < mid_name
@@ -50,8 +45,6 @@ class AddressBook
         lower = mid + 1
       end
     end
-
-    # #5
     return nil
   end
 
